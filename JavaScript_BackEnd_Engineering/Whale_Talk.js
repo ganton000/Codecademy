b9d465cae56308f0932cdcff044506836224327d
George Anton <SendittoGeorgeAnton@gmail.com>
2022-01-21 18:37:17 -0500

    Whale Talk Project

---
+++ JavaScript_BackEnd_Engineering/Whale_Talk.js
@@ -0,0 +1,26 @@
+/*
+"Whale Talk" -Cipher Project for JS Backend Engineering Career Path in Codecademy.
+*/
+
+let input = 'Hi, Human';
+const vowels = ['a','e','i','o','u'];
+
+let resultArray = [];
+
+for (let i =0; i<input.length; i++){
+  for (let j =0; j<vowels.length; j++){
+    if (input[i] === vowels[j]){
+      resultArray.push(input[i])
+    }
+  };
+  if (input[i] === 'e'){
+    resultArray.push(input[i])
+  };
+    if (input[i] === 'u'){
+    resultArray.push(input[i])
+  }
+}
+
+console.log(resultArray)
+
+console.log((resultArray.join('')).toUpperCase())
