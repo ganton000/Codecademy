9f7451d82a7248821afc6b00cfe7414ab7cc4365
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-06-23 22:43:54 -0400

    Added routes for api

--- JavaScript_BackEnd_Engineering/boss-machine/server.js
+++ JavaScript_BackEnd_Engineering/boss-machine/server.js
@@ -1,25 +1,31 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 module.exports = app;
 
-/* Do not change the following line! It is required for testing and allowing
-*  the frontend application to interact as planned with the api server
-*/
 const PORT = process.env.PORT || 4001;
 
-// Add middleware for handling CORS requests from index.html
+//enable all cors requests
+app.use(cors());
+
+//parse application/json
+app.use(bodyParser.json());
 
 
-// Add middware for parsing request bodies here:
 
-
-// Mount your existing apiRouter below at the '/api' path.
+//Import router
 const apiRouter = require('./server/api');
+
+//Mount apiRouter
+app.use('/api', apiRouter)
 
 
 // This conditional is here for testing purposes:
-if (!module.parent) { 
+if (!module.parent) {
   // Add your code to start the server listening at PORT below:
-
+  app.listen(PORT, () => {
+    console.log(`CORS-enabled web server is now listening on PORT: ${PORT}...`)
+  })
 }
